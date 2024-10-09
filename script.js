97c09f5dee6280aaa8096ea992a711bb50f6c7ed
Christian <christianyuen1@gmail.com>
2024-10-09 17:23:51 -0600

    added moving gif for audio unmute

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ muteBtn.addEventListener('click', () => {
   if (video.muted === true) {
     // Unmute
     video.muted = false;
-    muteBtn.innerHTML = "<img class='mute' src='./assets/muted.png' alt='yourimgtext'/>"
+    muteBtn.innerHTML = "<img class='mute' src='./assets/muted.png' alt=''/>"
   } else {
     // Mute
     video.muted = true;
-    muteBtn.innerHTML = "<img class='unmute' src='./assets/sound.png' alt='yourimgtext'/>"
+    muteBtn.innerHTML = "<img class='unmute' src='./assets/audio.gif' alt=''/>"
   } 
 });

bb13ece6dcb3eb5c5159c22ebdbf06340042d040
Christian <christianyuen1@gmail.com>
2024-10-09 18:17:57 -0600

    changed font

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ muteBtn.addEventListener('click', () => {
   if (video.muted === true) {
     // Unmute
     video.muted = false;
-    muteBtn.innerHTML = "<img class='mute' src='./assets/muted.png' alt=''/>"
+    // muteBtn.innerHTML = "<img class='mute' src='./assets/muted.png' alt=''/>"
+    muteBtn.innerHTML = "<button><p>MUTE</p></button"
   } else {
     // Mute
     video.muted = true;
-    muteBtn.innerHTML = "<img class='unmute' src='./assets/audio.gif' alt=''/>"
+    // muteBtn.innerHTML = "<img class='unmute' src='./assets/audio.gif' alt=''/>"
+    muteBtn.innerHTML = "<button><p>UNMUTE</p></button>"
   } 
 });

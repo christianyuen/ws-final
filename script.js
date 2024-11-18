3265e15ca6b496893d5eb2b28f6ebc45fd63b6c1
Christian <christianyuen1@gmail.com>
2024-11-18 14:55:07 -0500

    changed mute/unmute to audio icon

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ muteBtn.addEventListener('click', () => {
   if (video.muted === true) {
     // Unmute
     video.muted = false;
-    // muteBtn.innerHTML = "<img class='mute' src='./assets/muted.png' alt=''/>"
-    muteBtn.innerHTML = "<button><p>MUTE</p></button"
+    muteBtn.innerHTML = "<img class='mute' src='./assets/muted.png' alt=''/>"
+    // muteBtn.innerHTML = "<button><p>MUTE</p></button"
   } else {
     // Mute
     video.muted = true;
-    // muteBtn.innerHTML = "<img class='unmute' src='./assets/audio.gif' alt=''/>"
-    muteBtn.innerHTML = "<button><p>UNMUTE</p></button>"
+    muteBtn.innerHTML = "<img class='unmute' src='./assets/un-mute.png' alt=''/>"
+    // muteBtn.innerHTML = "<button><p>UNMUTE</p></button>"
   } 
 });
